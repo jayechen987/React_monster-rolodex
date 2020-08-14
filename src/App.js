f23413e7102fd7144143b7f0896530bc306164dd
jayechen987 <jayechen987@gmail.com>
2020-08-14 15:49:44 +0800

    test change

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBox
-          placeholder="search monsters"
+          placeholder="search monstersssss"
           handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters}></CardList>
