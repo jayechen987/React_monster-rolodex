bf9d48f1e4eb3890cf14964ca6f9edd2a45b2d8d
jayechen987 <jayechen987@gmail.com>
2020-08-18 14:39:19 +0800

    Updates

---
+++ precache-manifest.eefdbe2eccf27839c9b742f0eeb5d0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4021b20a0a586c2e2d60bfd8317d4b67",
+    "url": "/React_monster-rolodex/index.html"
+  },
+  {
+    "revision": "5765eeef872fc7b759be",
+    "url": "/React_monster-rolodex/static/css/main.617a6a4b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/React_monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5765eeef872fc7b759be",
+    "url": "/React_monster-rolodex/static/js/main.6532c93e.chunk.js"
+  },
+  {
+    "revision": "8747a7a2fdb6bf2aa152",
+    "url": "/React_monster-rolodex/static/js/runtime-main.c58da037.js"
+  }
+]);
